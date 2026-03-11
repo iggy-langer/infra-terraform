5c6ff6d031554e0f257f028679fac299fbd99bd2
iggy-langer <262610318+iggy-langer@users.noreply.github.com>
2026-03-12 00:06:07 +1000

    Migrate to new database schema

--- parser.js
+++ parser.js
@@ -3,3 +3,8 @@ const capitalizeFirstLetter = (string) => {
   return string.charAt(0).toUpperCase() + string.slice(1);
 };
 
+
+const formatDate = (date) => {
+  return new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(date));
+};
+
